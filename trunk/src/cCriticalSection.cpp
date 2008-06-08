c6d4501f41bcb29c18e4537e10a0a5086ffceb25
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-06-08 15:29:53 +0000

    PTHREAD_MUTEX_RECURSIVE_NP should be replaced by PTHREAD_MUTEX_RECURSIVE under Darwin

--- trunk/src/cCriticalSection.cpp
+++ trunk/src/cCriticalSection.cpp
@@ -22,26 +22,27 @@ namespace TA3D
 {
 	void cCriticalSection::CreateCS()
 	{
-#if defined TA3D_PLATFORM_WINDOWS
-		::InitializeCriticalSection( &m_hCritSection );
-#else
+        #ifdef TA3D_PLATFORM_WINDOWS
+		::InitializeCriticalSection(&m_hCritSection);
+        #else
 		pthread_mutexattr_t mutexattr;
-
 		pthread_mutexattr_init(&mutexattr);
+        # ifdef TA3D_PLATFORM_DARWIN
+		pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
+        # else
 		pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE_NP);
-
+        # endif
 		::pthread_mutex_init( &m_hCritSection, &mutexattr );
-
 		pthread_mutexattr_destroy(&mutexattr);
-#endif
+        #endif
 	}
 
 	void cCriticalSection::DeleteCS()
 	{
-#if defined TA3D_PLATFORM_WINDOWS
+        #ifdef TA3D_PLATFORM_WINDOWS
 		::DeleteCriticalSection( &m_hCritSection );
-#else
+        #else
 		::pthread_mutex_unlock( &m_hCritSection );
-#endif
+        #endif
 	}
 } // namespace TA3D
