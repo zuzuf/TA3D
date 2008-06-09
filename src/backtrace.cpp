f1e986882fac3e40d4c66a7566f4c5f38eca8b75
Damien GERARD <milipili@users.noreply.github.com>
2008-06-09 14:46:32 +0000

    Completely removed signals handlers for OS Xi (via the define TA3D_PLATFORM_DARWIN) to be able to quit the program. Anyway the system already produces a crash report (accessible from end-users - they must click on `Report` and copy/paste the logs) when the program crashes.
    More informations are available here : 
    http://developer.apple.com/technotes/tn2004/tn2123.html

--- src/backtrace.cpp
+++ src/backtrace.cpp
@@ -15,104 +15,135 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
 
+
+
+
+// Signals should be disabled under OS X, since the system already produces a crash report
+// More information are available here :
+// http://developer.apple.com/technotes/tn2004/tn2123.html
+#ifndef TA3D_PLATFORM_DARWIN
+
+
 #include "stdafx.h"
 #include "TA3D_NameSpace.h"
 
 #ifdef TA3D_PLATFORM_LINUX
-#define TA3D_BACKTRACE_SUPPORT
+#   define TA3D_BACKTRACE_SUPPORT
 #endif
 
 #ifdef TA3D_BACKTRACE_SUPPORT
-#include <execinfo.h>
+#   include <execinfo.h>
 #endif
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Obtain a backtrace and print it to stdout.
+
+
+
+/*!
+ * \brief Obtain a backtrace and print it to stdout.
+ *
+ * Only if TA3D_BACKTRACE_SUPPORT is defined, a backtrace is obtained
+ * then writen into a log file. It will be displayed in stdout in any case.
+ * After this call, the program will exit with a exit status code equals
+ * to `-1`.
+ *
+ * \param signum Which signal was received
+ */
 void backtrace_handler (int signum)
 {
+    # ifdef TA3D_BACKTRACE_SUPPORT
+
+    // Retrieving a backtrace
 	void *array[400];
-	size_t size;
-	char **strings;
-	size_t i;
+	size_t size = backtrace (array, 400);
+	char** strings = backtrace_symbols(array, size);
 
-#ifdef TA3D_BACKTRACE_SUPPORT
-	std::ofstream   m_File;
-
-	m_File.open( (TA3D_OUTPUT_DIR + "backtrace.txt").c_str(), std::ios::out | std::ios::trunc );
-
-	if( m_File.is_open() ) {
+    // Try to log it
+	std::ofstream m_File;
+	m_File.open((TA3D_OUTPUT_DIR + "backtrace.txt").c_str(), std::ios::out | std::ios::trunc);
+	if(m_File.is_open())
+    {
 		m_File << "received signal " << strsignal( signum ) << "\n";
-
-		size = backtrace (array, 400);
-		strings = backtrace_symbols (array, size);
-
 		m_File << "Obtained " << size << " stack frames.\n";
-
-		for (i = 0; i < size; i++)				// Write to the file
+		for (int i = 0; i < size; ++i)
 			m_File << strings[i] << "\n";
-
 		m_File.flush();
 		m_File.close();
 
-		printf("received signal %s\n", strsignal( signum ) );		// Print so we don't have to open the file to look at this
+		printf("received signal %s\n", strsignal( signum ));
 		printf ("Obtained %zd stack frames.\n", size);
-		for (i = 0; i < size; i++)
+		for (int i = 0; i < size; ++i)
 			printf ("%s\n", strings[i]);
 
-		free (strings);
+		String szErrReport = "An error has occured.\nDebugging information have been logged to:\n"
+            + TA3D_OUTPUT_DIR
+            + "backtrace.txt\nPlease report to our forums (http://ta3d.darkstars.co.uk/)\nand keep this file, it'll help us debugging.\n";
 
-		String szErrReport = "An error has occured.\nDebugging information have been logged to:\n" + TA3D_OUTPUT_DIR + "backtrace.txt\nPlease report to \
-our forums (http://ta3d.darkstars.co.uk/)\nand keep this file, it'll help us debugging.\n";
-
-#ifdef TA3D_PLATFORM_WINDOWS
-		::MessageBoxA( NULL, szErrReport.c_str(), "TA3D Application Error", MB_OK  | MB_TOPMOST | MB_ICONERROR );
-#else
-		allegro_init();
-		allegro_message( szErrReport.c_str() );
-		allegro_exit();
-#endif
-		}
-	else {
-		printf("received signal %s\n", strsignal( signum ) );
+        # ifdef TA3D_PLATFORM_WINDOWS
+		    ::MessageBoxA( NULL, szErrReport.c_str(), "TA3D Application Error", MB_OK  | MB_TOPMOST | MB_ICONERROR );
+        # else
+		    allegro_init();
+		    allegro_message( szErrReport.c_str() );
+		    allegro_exit();
+        # endif // TA3D_PLATFORM_WINDOWS
+	}
+	else
+    {
+        // The file is not opened
+        // The backtrace will be directly to stdout instead.
+		printf("received signal %s\n", strsignal(signum));
 		printf("couldn't open file for writing!!\n");
-		
-		size = backtrace (array, 400);
-		strings = backtrace_symbols (array, size);
-
 		printf ("Obtained %zd stack frames.\n", size);
-
-		for (i = 0; i < size; i++)
+		for (int i = 0; i < size; ++i)
 			printf ("%s\n", strings[i]);
+	}
+	free(strings);
 
-		free (strings);
-		}
-#else
-	String szErrReport = "An error has occured.\nDebugging information could not be logged because this isn't\nnot supported for your system.\nPlease report to \
+    #else // ifdef TA3D_BACKTRACE_SUPPORT
+
+        // The backtrace support is disabled: warns the user
+	    String szErrReport = "An error has occured.\nDebugging information could not be logged because this isn't\nnot supported for your system.\nPlease report to \
 our forums (http://ta3d.darkstars.co.uk/) so we can fix it.";
-#ifdef TA3D_PLATFORM_WINDOWS
-	::MessageBoxA( NULL, szErrReport.c_str(), "TA3D Application Error", MB_OK  | MB_TOPMOST | MB_ICONERROR );
-#endif
-#ifdef TA3D_PLATFORM_LINUX
-	allegro_init();
-	allegro_message( szErrReport.c_str() );
-	allegro_exit();
-#endif
-#endif
+        # ifdef TA3D_PLATFORM_WINDOWS
+	        ::MessageBoxA( NULL, szErrReport.c_str(), "TA3D Application Error", MB_OK  | MB_TOPMOST | MB_ICONERROR );
+        # else
+	        allegro_init();
+	        allegro_message( szErrReport.c_str() );
+	        allegro_exit();
+        # endif // ifdef TA3D_PLATFORM_LINUX
+    
+    #endif // ifdef TA3D_BACKTRACE_SUPPORT
 	exit(-1);
 }
 
+#endif // ifdef TA3D_PLATFORM_DARWIN
+
+
+
+
+
 int init_signals (void)
 {
-#ifdef TA3D_PLATFORM_WINDOWS
+    // Signals should be disabled under OS X, since the system already produces a crash report
+    // More information are available here :
+    // http://developer.apple.com/technotes/tn2004/tn2123.html
+    #ifndef TA3D_PLATFORM_DARWIN
+
+    # ifdef TA3D_PLATFORM_WINDOWS
 	int signum[] = { SIGFPE, SIGILL, SIGSEGV, SIGABRT };
 	int nb_signals = 4;
-#else
+    # else
 	int signum[] = { SIGFPE, SIGILL, SIGSEGV, SIGBUS, SIGABRT, SIGIOT, SIGTRAP, SIGSYS };
 	int nb_signals = 8;
-#endif
+    # endif // ifdef TA3D_PLATFORM_WINDOWS
 	for( int i = 0 ; i < nb_signals ; i++ )
 		if (signal (signum[i], backtrace_handler) == SIG_IGN)
 			signal (signum[i], SIG_IGN);
+
+    #endif // ifdef TA3D_PLATFORM_DARWIN
 }
+
+
