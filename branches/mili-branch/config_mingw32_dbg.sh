4e8b5be4e3b91a874fb768d9b5fef1c445e6db6f
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-06-12 20:32:39 +0000

    Removed autotools

--- branches/mili-branch/config_mingw32_dbg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./configure --prefix=/usr/i586-mingw32msvc --host=i586-mingw32msvc --target=i586-mingw32msvc --with-msw --enable-mingw32 --disable-allegrotest CXXFLAGS="-g -O0 -ffast-math -march=i686"
