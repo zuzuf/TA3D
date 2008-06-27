cb69ae07bc9b954746f434207402eacfee98b1f6
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-06-27 20:46:12 +0000

    Typo

--- branches/mili-branch/src/threads/mutex.cpp
+++ branches/mili-branch/src/threads/mutex.cpp
@@ -17,7 +17,7 @@ namespace TA3D
         # else
 		pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE_NP);
         # endif
-		::pthread_mutex_init(&pPthreadLock, &mutexattr);
+		pthread_mutex_init(&pPthreadLock, &mutexattr);
 		pthread_mutexattr_destroy(&mutexattr);
         #endif
     }
