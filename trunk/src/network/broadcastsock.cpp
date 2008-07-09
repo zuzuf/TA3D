0c56ff60a0470dd1295b76adbd60ff003362082d
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-07-09 20:20:24 +0000

    Moved the class into the namespace TA3D

--- trunk/src/network/broadcastsock.cpp
+++ trunk/src/network/broadcastsock.cpp
@@ -15,171 +15,174 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
 
-#include "../stdafx.h"
+#include "broadcastsock.h"
 #include "../TA3D_NameSpace.h"
 
-/***********************************/
-/**  methods for BroadcastSock  ****/
-/***********************************/
 
-
-int BroadcastSock::Open(const char* port)
+namespace TA3D
 {
-	udpsocket.Open(NULL,port,PROTOCOL_BROADCAST);
-	
-	if( !udpsocket.isOpen()  )
+
+
+
+    int BroadcastSock::Open(const char* port)
     {
-		Console->AddEntry("couldn't open UDP socket for broadcasting" );
-		//one of them didnt work... quit
-		udpsocket.Close();
-		return -1;
-	}
-	
-	obp = 0;
-	uibp = 0;
-	uiremain = -1;
-	return 0;
-}
+        udpsocket.Open(NULL,port,PROTOCOL_BROADCAST);
 
-
-
-int BroadcastSock::isOpen()
-{
-	return udpsocket.isOpen();
-}
-
-void BroadcastSock::Close()
-{
-	udpsocket.Close();
-}
-
-
-
-//byte shuffling
-void BroadcastSock::loadLong(uint32_t x)//uint32
-{
-	uint32_t temp;
-	temp = nlSwapl(x);
-	memcpy(outbuf+obp,&temp,4);
-	obp += 4;
-}
-
-void BroadcastSock::loadShort(uint16_t x)//uint16
-{
-	uint16_t temp;
-	temp = nlSwaps(x);
-	memcpy(outbuf+obp,&temp,2);
-	obp += 2;
-}
-
-void BroadcastSock::loadByte(uint8_t x){//uint8
-	memcpy(outbuf+obp,&x,1);
-	obp += 1;
-}
-
-void BroadcastSock::loadString(const char* x)//null terminated
-{
-	int n = strlen(x);
-	if(n < 256){
-		memcpy(outbuf+obp,x,n);
-		obp+=n;
-	}
-	else{
-		memcpy(outbuf+obp,x,256);
-		obp+=256;
-	}
-	loadByte('\0');
-}	
-
-void BroadcastSock::loadFloat(float x)
-{
-	float temp;
-	temp = nlSwapf(x);
-	memcpy(outbuf+obp,&temp,4);
-	obp += 4;
-}
-
-
-void BroadcastSock::sendUDP()
-{
-    if( !udpsocket.isOpen() )
-    {
-        Console->AddEntry("WARNING: broadcast socket closed!!");
-    }
-    int n = 0;
-    while(n!=obp)
-    {
-        int v = udpsocket.Send(outbuf,obp);
-        if( v <= 0 )
+        if( !udpsocket.isOpen()  )
         {
-            Console->AddEntry("ERROR : couldn't broadcast data");
-            break;
+            Console->AddEntry("couldn't open UDP socket for broadcasting" );
+            //one of them didnt work... quit
+            udpsocket.Close();
+            return -1;
         }
-        n += v;
-    }
-    obp = 0;
-}
 
-void BroadcastSock::recvUDP()
-{
-    if(uiremain == 0)
-        return;
-    memset( udpinbuf, 0, MULTICAST_BUFFER_SIZE );
-    int p = udpsocket.Recv(udpinbuf,MULTICAST_BUFFER_SIZE);//get new number
-    if( p <= 0 ) {
+        obp = 0;
+        uibp = 0;
         uiremain = -1;
-        return;
-    }
-    uibp = p;
-    uiremain = 0;
-}
-
-
-void BroadcastSock::pumpIn()
-{
-    recvUDP();
-}
-
-char BroadcastSock::getPacket()
-{
-    if(uiremain>0)
         return 0;
-    return udpinbuf[0];
-}
+    }
 
 
 
-int BroadcastSock::takeFive(const int time)
-{
-    return udpsocket.takeFive(time);
-}
+    int BroadcastSock::isOpen()
+    {
+        return udpsocket.isOpen();
+    }
+
+    void BroadcastSock::Close()
+    {
+        udpsocket.Close();
+    }
 
 
-String BroadcastSock::getAddress()
-{
-    return udpsocket.getNumber();
-}
+
+    //byte shuffling
+    void BroadcastSock::loadLong(uint32_t x)//uint32
+    {
+        uint32_t temp;
+        temp = nlSwapl(x);
+        memcpy(outbuf+obp,&temp,4);
+        obp += 4;
+    }
+
+    void BroadcastSock::loadShort(uint16_t x)//uint16
+    {
+        uint16_t temp;
+        temp = nlSwaps(x);
+        memcpy(outbuf+obp,&temp,2);
+        obp += 2;
+    }
+
+    void BroadcastSock::loadByte(uint8_t x){//uint8
+        memcpy(outbuf+obp,&x,1);
+        obp += 1;
+    }
+
+    void BroadcastSock::loadString(const char* x)//null terminated
+    {
+        int n = strlen(x);
+        if(n < 256){
+            memcpy(outbuf+obp,x,n);
+            obp+=n;
+        }
+        else{
+            memcpy(outbuf+obp,x,256);
+            obp+=256;
+        }
+        loadByte('\0');
+    }	
+
+    void BroadcastSock::loadFloat(float x)
+    {
+        float temp;
+        temp = nlSwapf(x);
+        memcpy(outbuf+obp,&temp,4);
+        obp += 4;
+    }
 
 
-int BroadcastSock::sendMessage( const char* msg )
-{
-    loadString((char*)msg);
-    sendUDP();
-    return 0;
-}
+    void BroadcastSock::sendUDP()
+    {
+        if( !udpsocket.isOpen() )
+        {
+            Console->AddEntry("WARNING: broadcast socket closed!!");
+        }
+        int n = 0;
+        while(n!=obp)
+        {
+            int v = udpsocket.Send(outbuf,obp);
+            if( v <= 0 )
+            {
+                Console->AddEntry("ERROR : couldn't broadcast data");
+                break;
+            }
+            n += v;
+        }
+        obp = 0;
+    }
 
-String BroadcastSock::makeMessage()
-{
-    if(uiremain != 0)
-        return "";
-    String msg;
-    msg.reserve( uibp + 1 );
-    for( int i = 0 ; i < uibp && udpinbuf[ i ] != 0 ; i++ )
-        msg += udpinbuf[ i ];
-    uibp = 0;
-    uiremain = -1;
+    void BroadcastSock::recvUDP()
+    {
+        if(uiremain == 0)
+            return;
+        memset( udpinbuf, 0, MULTICAST_BUFFER_SIZE );
+        int p = udpsocket.Recv(udpinbuf,MULTICAST_BUFFER_SIZE);//get new number
+        if( p <= 0 ) {
+            uiremain = -1;
+            return;
+        }
+        uibp = p;
+        uiremain = 0;
+    }
 
-    return msg;
-}
 
+    void BroadcastSock::pumpIn()
+    {
+        recvUDP();
+    }
+
+    char BroadcastSock::getPacket()
+    {
+        if(uiremain>0)
+            return 0;
+        return udpinbuf[0];
+    }
+
+
+
+    int BroadcastSock::takeFive(const int time)
+    {
+        return udpsocket.takeFive(time);
+    }
+
+
+    String BroadcastSock::getAddress()
+    {
+        return udpsocket.getNumber();
+    }
+
+
+    int BroadcastSock::sendMessage( const char* msg )
+    {
+        loadString((char*)msg);
+        sendUDP();
+        return 0;
+    }
+
+    String BroadcastSock::makeMessage()
+    {
+        if(uiremain != 0)
+            return "";
+        String msg;
+        msg.reserve( uibp + 1 );
+        for( int i = 0 ; i < uibp && udpinbuf[ i ] != 0 ; i++ )
+            msg += udpinbuf[ i ];
+        uibp = 0;
+        uiremain = -1;
+
+        return msg;
+    }
+
+
+} // namespace TA3D
 
