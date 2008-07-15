591a58304b095429a7a9036fcc7077f90d8e5ded
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-07-15 15:53:28 +0000

    The current revision is now displayed into the logs, given by CMake

--- trunk/config.h.cmake
+++ trunk/config.h.cmake
@@ -1,16 +1,36 @@
-#ifndef _CONFIG_H
-#define _CONFIG_H
+#ifndef __CONFIG_H__
+# define __CONFIG_H__
 
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
 
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "@PACKAGE_STRING@"
+//! Define to the address where bug reports for this package should be sent
+# define PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
 
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "@PACKAGE_VERSION@"
+//! Define to the full name and version of this package
+# define PACKAGE_STRING "@PACKAGE_STRING@"
 
-/* Define to the version string of this package. */
-#define TA3D_ENGINE_VERSION "@TA3D_ENGINE_VERSION@"
+//! Define to the version of this package
+# define PACKAGE_VERSION "@PACKAGE_VERSION@"
 
-#endif
+//! Define to the version string of this package
+# define TA3D_ENGINE_VERSION "@TA3D_ENGINE_VERSION@"
+
+
+//! The hi version for TA3D
+# define TA3D_VERSION_HI  @TA3D_VERSION_HI@
+//! The lo version for TA3D
+# define TA3D_VERSION_LO  @TA3D_VERSION_LO@
+//! Tag for this version
+# define TA3D_VERSION_TAG "@TA3D_VERSION_TAG@"
+
+//! The current revision
+# define TA3D_CURRENT_REVISION   @SVN_REV@
+
+//! The website
+# define TA3D_WEBSITE                "@TA3D_WEBSITE@"
+//! The forum
+# define TA3D_WEBSITE_FORUMS         "@TA3D_WEBSITE_FORUM@"
+//! URL for a new bug report
+# define TA3D_WEBSITE_NEW_BUGREPORT  "@TA3D_WEBSITE_NEW_BUGREPORT@"
+
+
+#endif // __CONFIG_H__
