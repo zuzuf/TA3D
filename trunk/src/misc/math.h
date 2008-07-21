1c3b844f5bd0e46c5d9a9a44a87fd09daa84e06e
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-07-21 14:39:38 +0000

    Moved defines into the appropriated header

--- trunk/src/misc/math.h
+++ trunk/src/misc/math.h
@@ -76,4 +76,57 @@ namespace Math
 
 
 
+
+// Pi, our favorite number
+# ifdef PI
+#   undef PI
+# endif
+# define PI 3.141592653589793238462643383279502884197169399375105f
+# define DB_PI 6.28318530717958647693f
+
+// Square root of 2
+# ifdef SQRT2
+#   undef SQRT2
+# endif
+# define SQRT2 1.414213562373095048801688724209698078569671875376948f
+
+// Square root of 2 over 2
+# ifdef SQRT2OVER2
+#   undef SQRT2OVER2
+# endif
+# define SQRT2OVER2 0.707106781186547524400844362104849039284835937688474f
+
+// Square root of 3
+# ifdef SQRT3
+#   undef SQRT3
+# endif
+# define SQRT3 1.732050807568877293527446341505872366942805253810381f
+
+// Square root of 3 over 2
+# ifdef SQRT3OVER2
+#   undef SQRT3OVER2
+# endif
+# define SQRT3OVER2 0.866025403784438646763723170752936183471402626905190f
+
+// The natural number "e"
+# ifdef NAT_E
+#   undef NAT_E
+# endif
+# define NAT_E 2.718281828459045235360287471352662497757247093699959f
+
+
+# define DEG2RAD  (PI / 180.0f)
+# define RAD2DEG  (180.0f / PI)
+
+# define DEG2TA   (65536.0f / 360.0f)
+# define TA2DEG   (360.0f / 65536.0f)
+
+# define RAD2TA   (RAD2DEG * DEG2TA)
+# define TA2RAD   (TA2DEG  * DEG2RAD)
+
+# define I2PWR16  (1.0f / 65536.0f)
+
+
+
+
 #endif // __TA3D_XX__MATH_H__
