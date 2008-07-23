0bc443dbe561731664759865dd3616c3d2fcc6de
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2008-07-23 08:25:45 +0000

    Typo + code rearrangement

--- trunk/src/scripts/cob.cpp
+++ trunk/src/scripts/cob.cpp
@@ -16,63 +16,145 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
 
 /*-----------------------------------------------------------------------------------\
-|                                         cob.cpp                                    |
-|  ce fichier contient les structures, classes et fonctions nécessaires à la lecture |
-| et à l'éxecution des fichiers cob du jeu totalannihilation qui sont les scripts    |
-| qui permettent d'animer les modèles 3d du jeu.(cela inclus également les classes   |
-| de gestion des unités).                                                            |
-|                                                                                    |
-\-----------------------------------------------------------------------------------*/
+  |                                         cob.cpp                                    |
+  |  ce fichier contient les structures, classes et fonctions nécessaires à la lecture |
+  | et à l'éxecution des fichiers cob du jeu totalannihilation qui sont les scripts    |
+  | qui permettent d'animer les modèles 3d du jeu.(cela inclus également les classes   |
+  | de gestion des unités).                                                            |
+  |                                                                                    |
+  \-----------------------------------------------------------------------------------*/
 
-#include "../stdafx.h"
+#include "cob.h"
 #include "../misc/matrix.h"				// Some math routines
 #include "../TA3D_NameSpace.h"
 #include "../ta3dbase.h"
-#include "cob.h"
 
-	void SCRIPT::load_cob(byte *data)
-	{
-		destroy();				// Au cas où
 
-		COBHeader header;
-		header.VersionSignature=*((int*)data);
-		header.NumberOfScripts=*((int*)(data+4));
-		header.NumberOfPieces=*((int*)(data+8));
-		header.Unknown_0=*((int*)(data+12));
-		header.Unknown_1=*((int*)(data+16));
-		header.Unknown_2=*((int*)(data+20));
-		header.OffsetToScriptCodeIndexArray=*((int*)(data+24));
-		header.OffsetToScriptNameOffsetArray=*((int*)(data+28));
-		header.OffsetToPieceNameOffsetArray=*((int*)(data+32));
-		header.OffsetToScriptCode=*((int*)(data+36));
-		header.Unknown_3=*((int*)(data+40));
+namespace TA3D
+{
+
+    /*!
+     * \brief Definition of a Header of a COB file
+     */
+    struct COBHeader
+    {
+        //!
+        int VersionSignature;
+        //!
+        int NumberOfScripts;
+        //!
+        int NumberOfPieces;
+
+        //!
+        int Unknown_0;
+        //!
+        int Unknown_1;
+        //! Always seems to be 0
+        int Unknown_2;
+
+        //!
+        int OffsetToScriptCodeIndexArray;
+        //!
+        int OffsetToScriptNameOffsetArray;
+        //!
+        int OffsetToPieceNameOffsetArray;
+        //!
+        int OffsetToScriptCode;
+
+        //! Always seems to point to first script name
+        int Unknown_3;
+
+    }; // class COBHeader
+
+
+
+
+    void SCRIPT::load_cob(byte *data)
+    {
+        destroy();				// Au cas où
+
+        COBHeader header;
+        header.VersionSignature = *((int*)data);
+        header.NumberOfScripts = *((int*)(data + 4));
+        header.NumberOfPieces = *((int*)(data + 8));
+        header.Unknown_0 = *((int*)(data + 12));
+        header.Unknown_1 = *((int*)(data + 16));
+        header.Unknown_2 = *((int*)(data + 20));
+        header.OffsetToScriptCodeIndexArray  = *((int*)(data + 24));
+        header.OffsetToScriptNameOffsetArray = *((int*)(data + 28));
+        header.OffsetToPieceNameOffsetArray = *((int*)(data + 32));
+        header.OffsetToScriptCode = *((int*)(data + 36));
+        header.Unknown_3 = *((int*)(data + 40));
 
 #ifdef DEBUG_MODE
-/*		printf("header.NumberOfScripts=%d\n",header.NumberOfScripts);
-		printf("header.NumberOfPieces=%d\n",header.NumberOfPieces);
-		printf("header.OffsetToScriptCodeIndexArray=%d\n",header.OffsetToScriptCodeIndexArray);
-		printf("header.header.OffsetToScriptNameOffsetArray=%d\n",header.OffsetToScriptNameOffsetArray);
-		printf("header.OffsetToPieceNameOffsetArray=%d\n",header.OffsetToPieceNameOffsetArray);
-		printf("header.OffsetToScriptCode=%d\n",header.OffsetToScriptCode);*/
+        /*		printf("header.NumberOfScripts=%d\n",header.NumberOfScripts);
+                printf("header.NumberOfPieces=%d\n",header.NumberOfPieces);
+                printf("header.OffsetToScriptCodeIndexArray=%d\n",header.OffsetToScriptCodeIndexArray);
+                printf("header.header.OffsetToScriptNameOffsetArray=%d\n",header.OffsetToScriptNameOffsetArray);
+                printf("header.OffsetToPieceNameOffsetArray=%d\n",header.OffsetToPieceNameOffsetArray);
+                printf("header.OffsetToScriptCode=%d\n",header.OffsetToScriptCode);*/
 #endif
 
-		nb_script=header.NumberOfScripts;
-		nb_piece=header.NumberOfPieces;
-		name=(char**) malloc(sizeof(char*)*nb_script);
-		piece_name=(char**) malloc(sizeof(char*)*nb_piece);
+        nb_script=header.NumberOfScripts;
+        nb_piece=header.NumberOfPieces;
+        name=(char**) malloc(sizeof(char*)*nb_script);
+        piece_name=(char**) malloc(sizeof(char*)*nb_piece);
 
-		int f_pos=header.OffsetToScriptNameOffsetArray;
-		int i;
-		for(i=0;i<nb_script;i++)
-			name[i]=strdup((char*)(data+(*((int*)(data+f_pos+4*i)))));
-		f_pos=header.OffsetToPieceNameOffsetArray;
-		for(i=0;i<nb_piece;i++)
-			piece_name[i]=strdup((char*)(data+(*((int*)(data+f_pos+4*i)))));
-		Data=data;
-		script_code=(int**) malloc(sizeof(int*)*nb_script);
-		dec_offset=(int*) malloc(sizeof(int)*nb_script);
-		for(i=0;i<nb_script;i++) {
-			dec_offset[i]=(*((int*)(data+header.OffsetToScriptCodeIndexArray+4*i)));
-			script_code[i]=(int*)(data+header.OffsetToScriptCode+4*(*((int*)(data+header.OffsetToScriptCodeIndexArray+4*i))));
-			}
-	}
+        int f_pos = header.OffsetToScriptNameOffsetArray;
+        int i;
+        for (i = 0; i < nb_script; ++i)
+            name[i]=strdup((char*)(data+(*((int*)(data+f_pos+4*i)))));
+        f_pos=header.OffsetToPieceNameOffsetArray;
+        for(i = 0; i < nb_piece; ++i)
+            piece_name[i] = strdup((char*)(data+(*((int*)(data+f_pos+4*i)))));
+        Data=data;
+        script_code=(int**) malloc(sizeof(int*)*nb_script);
+        dec_offset=(int*) malloc(sizeof(int)*nb_script);
+        for (i = 0; i < nb_script; ++i)
+        {
+            dec_offset[i]  = (*((int*)(data + header.OffsetToScriptCodeIndexArray + 4 * i)));
+            script_code[i] = (int*)(data + header.OffsetToScriptCode
+                                    + 4 * (*((int*)(data + header.OffsetToScriptCodeIndexArray + 4 * i))));
+        }
+    }
+
+
+
+
+    void SCRIPT::destroy()
+    {
+        if (name)
+        {
+            for (int i = 0; i < nb_script; ++i)
+                free(name[i]);
+            free(name);
+        }
+        if (script_code)
+            free(script_code);
+        if (dec_offset)
+            free(dec_offset);
+        if (Data)
+            delete[] Data;
+        if (piece_name)
+        {
+            for (int i = 0; i < nb_piece; ++i)
+                free(piece_name[i]);
+            free(piece_name);
+        }
+        init();
+    }
+
+    void SCRIPT::init()
+    {
+        Data = NULL;
+        nb_script = 0;
+        nb_piece = 0;
+        script_code = NULL;
+        name = NULL;
+        piece_name = NULL;
+        dec_offset = NULL;
+    }
+
+
+
+} // namespace TA3D
--- trunk/src/scripts/cob.h
+++ trunk/src/scripts/cob.h
@@ -16,182 +16,144 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
 
 /*-----------------------------------------------------------------------------------\
-|                                         cob.h                                      |
-|  ce fichier contient les structures, classes et fonctions nécessaires à la lecture |
-| et à l'éxecution des fichiers cob du jeu totalannihilation qui sont les scripts    |
-| qui permettent d'animer les modèles 3d du jeu.(cela inclus également les classes   |
-| de gestion des unités).                                                            |
-|                                                                                    |
-\-----------------------------------------------------------------------------------*/
+  |                                         cob.h                                      |
+  |  ce fichier contient les structures, classes et fonctions nécessaires à la lecture |
+  | et à l'éxecution des fichiers cob du jeu totalannihilation qui sont les scripts    |
+  | qui permettent d'animer les modèles 3d du jeu.(cela inclus également les classes   |
+  | de gestion des unités).                                                            |
+  |                                                                                    |
+  \-----------------------------------------------------------------------------------*/
 
 #ifndef __CLASSE_COB
-#define __CLASSE_COB
+# define  __CLASSE_COB
 
-#define	SCRIPT_MOVE_OBJECT				0x10001000
-#define SCRIPT_WAIT_FOR_TURN			0x10011000
-#define SCRIPT_RANDOM_NUMBER			0x10041000
-#define SCRIPT_LESS						0x10051000
-#define SCRIPT_GREATER_EQUAL			0x10054000
-#define SCRIPT_GREATER					0x10053000
-#define SCRIPT_START_SCRIPT				0x10061000
-#define SCRIPT_EXPLODE					0x10071000
-#define SCRIPT_TURN_OBJECT				0x10002000
-#define SCRIPT_WAIT_FOR_MOVE			0x10012000
-#define SCRIPT_CREATE_LOCAL_VARIABLE	0x10022000
-#define SCRIPT_SUBTRACT					0x10032000
-#define SCRIPT_GET_VALUE_FROM_PORT		0x10042000
-#define SCRIPT_LESS_EQUAL				0x10052000
-#define SCRIPT_SPIN_OBJECT				0x10003000
-#define SCRIPT_SLEEP					0x10013000
-#define SCRIPT_MULTIPLY					0x10033000
-#define SCRIPT_CALL_SCRIPT				0x10063000
-#define SCRIPT_JUMP						0x10064000
-#define SCRIPT_SHOW_OBJECT				0x10005000
-#define SCRIPT_EQUAL					0x10055000
-#define SCRIPT_RETURN					0x10065000
-#define SCRIPT_NOT_EQUAL				0x10056000
-#define SCRIPT_IF						0x10066000
-#define SCRIPT_HIDE_OBJECT				0x10006000
-#define SCRIPT_SIGNAL					0x10067000
-#define SCRIPT_DONT_CACHE				0x10008000
-#define SCRIPT_SET_SIGNAL_MASK			0x10068000
-#define SCRIPT_NOT						0x1005A000
-#define SCRIPT_DONT_SHADE				0x1000E000
-#define SCRIPT_EMIT_SFX					0x1000F000
-#define SCRIPT_PUSH_CONST				0x10021001
-#define SCRIPT_PUSH_VAR					0x10021002
-#define SCRIPT_SET_VAR					0x10023002
-#define SCRIPT_PUSH_STATIC_VAR			0x10021004
-#define SCRIPT_SET_STATIC_VAR			0x10023004
-#define SCRIPT_OR						0x10036000
-#define SCRIPT_ADD						0x10031000  //added
-#define SCRIPT_STOP_SPIN				0x10004000  //added
-#define SCRIPT_DIVIDE					0x10034000  //added
-#define SCRIPT_MOVE_PIECE_NOW			0x1000B000  //added
-#define SCRIPT_TURN_PIECE_NOW			0x1000C000  //added
-#define SCRIPT_CACHE					0x10007000  //added
-#define SCRIPT_COMPARE_AND				0x10057000  //added
-#define SCRIPT_COMPARE_OR				0x10058000  //added
-#define SCRIPT_CALL_FUNCTION			0x10062000  //added
-#define SCRIPT_GET						0x10043000	//added
-#define SCRIPT_SET_VALUE				0x10082000	//added
-#define SCRIPT_ATTACH_UNIT				0x10083000	//added
-#define SCRIPT_DROP_UNIT				0x10084000	//added    
+# include "../stdafx.h"
 
-#define ACTIVATION			1	// set or get
-#define STANDINGMOVEORDERS	2	// set or get
-#define STANDINGFIREORDERS	3	// set or get
-#define HEALTH				4	// get (0-100%)
-#define INBUILDSTANCE		5	// set or get
-#define BUSY				6	// set or get (used by misc. special case missions like transport ships)
-#define PIECE_XZ			7	// get
-#define PIECE_Y				8	// get
-#define UNIT_XZ				9	// get
-#define	UNIT_Y				10	// get
-#define UNIT_HEIGHT			11	// get
-#define XZ_ATAN				12	// get atan of packed x,z coords
-#define XZ_HYPOT			13	// get hypot of packed x,z coords
-#define ATAN				14	// get ordinary two-parameter atan
-#define HYPOT				15	// get ordinary two-parameter hypot
-#define GROUND_HEIGHT		16	// get
-#define BUILD_PERCENT_LEFT	17	// get 0 = unit is built and ready, 1-100 = How much is left to build
-#define YARD_OPEN			18	// set or get (change which plots we occupy when building opens and closes)
-#define BUGGER_OFF			19	// set or get (ask other units to clear the area)
-#define ARMORED				20	// set or get
- 
-#define MIN_ID						69		// returns the lowest valid unit ID number
-#define MAX_ID						70		// returns the highest valid unit ID number
-#define MY_ID						71		// returns ID of current unit
-#define UNIT_TEAM					72		// returns team(player ID in TA) of unit given with parameter
-#define UNIT_BUILD_PERCENT_LEFT		73		// basically BUILD_PERCENT_LEFT, but comes with a unit parameter
-#define UNIT_ALLIED					74		// is unit given with parameter allied to the unit of the current COB script. 0=not allied, not zero allied
-#define UNIT_IS_ON_THIS_COMP		75		// indicates if the 1st parameter(a unit ID) is local to this computer
-#define VETERAN_LEVEL				32		// gets kills * 100
 
-struct COBHeader
+# define  SCRIPT_MOVE_OBJECT              0x10001000
+# define  SCRIPT_WAIT_FOR_TURN            0x10011000
+# define  SCRIPT_RANDOM_NUMBER            0x10041000
+# define  SCRIPT_LESS                     0x10051000
+# define  SCRIPT_GREATER_EQUAL            0x10054000
+# define  SCRIPT_GREATER                  0x10053000
+# define  SCRIPT_START_SCRIPT             0x10061000
+# define  SCRIPT_EXPLODE                  0x10071000
+# define  SCRIPT_TURN_OBJECT              0x10002000
+# define  SCRIPT_WAIT_FOR_MOVE            0x10012000
+# define  SCRIPT_CREATE_LOCAL_VARIABLE    0x10022000
+# define  SCRIPT_SUBTRACT                 0x10032000
+# define  SCRIPT_GET_VALUE_FROM_PORT      0x10042000
+# define  SCRIPT_LESS_EQUAL               0x10052000
+# define  SCRIPT_SPIN_OBJECT              0x10003000
+# define  SCRIPT_SLEEP                    0x10013000
+# define  SCRIPT_MULTIPLY                 0x10033000
+# define  SCRIPT_CALL_SCRIPT              0x10063000
+# define  SCRIPT_JUMP                     0x10064000
+# define  SCRIPT_SHOW_OBJECT              0x10005000
+# define  SCRIPT_EQUAL                    0x10055000
+# define  SCRIPT_RETURN                   0x10065000
+# define  SCRIPT_NOT_EQUAL                0x10056000
+# define  SCRIPT_IF                       0x10066000
+# define  SCRIPT_HIDE_OBJECT              0x10006000
+# define  SCRIPT_SIGNAL                   0x10067000
+# define  SCRIPT_DONT_CACHE               0x10008000
+# define  SCRIPT_SET_SIGNAL_MASK          0x10068000
+# define  SCRIPT_NOT                      0x1005A000
+# define  SCRIPT_DONT_SHADE               0x1000E000
+# define  SCRIPT_EMIT_SFX                 0x1000F000
+# define  SCRIPT_PUSH_CONST               0x10021001
+# define  SCRIPT_PUSH_VAR                 0x10021002
+# define  SCRIPT_SET_VAR                  0x10023002
+# define  SCRIPT_PUSH_STATIC_VAR          0x10021004
+# define  SCRIPT_SET_STATIC_VAR           0x10023004
+# define  SCRIPT_OR                       0x10036000
+# define  SCRIPT_ADD                      0x10031000  //added
+# define  SCRIPT_STOP_SPIN                0x10004000  //added
+# define  SCRIPT_DIVIDE                   0x10034000  //added
+# define  SCRIPT_MOVE_PIECE_NOW           0x1000B000  //added
+# define  SCRIPT_TURN_PIECE_NOW           0x1000C000  //added
+# define  SCRIPT_CACHE                    0x10007000  //added
+# define  SCRIPT_COMPARE_AND              0x10057000  //added
+# define  SCRIPT_COMPARE_OR               0x10058000  //added
+# define  SCRIPT_CALL_FUNCTION            0x10062000  //added
+# define  SCRIPT_GET                      0x10043000  //added
+# define  SCRIPT_SET_VALUE                0x10082000  //added
+# define  SCRIPT_ATTACH_UNIT              0x10083000  //added
+# define  SCRIPT_DROP_UNIT                0x10084000  //added    
+
+# define  ACTIVATION          1   // set or get
+# define  STANDINGMOVEORDERS  2   // set or get
+# define  STANDINGFIREORDERS  3   // set or get
+# define  HEALTH              4   // get (0-100%)
+# define  INBUILDSTANCE       5   // set or get
+# define  BUSY                6   // set or get (used by misc. special case missions like transport ships)
+# define  PIECE_XZ            7   // get
+# define  PIECE_Y             8   // get
+# define  UNIT_XZ             9   // get
+# define  UNIT_Y              10  // get
+# define  UNIT_HEIGHT         11  // get
+# define  XZ_ATAN             12  // get atan of packed x,z coords
+# define  XZ_HYPOT            13  // get hypot of packed x,z coords
+# define  ATAN                14  // get ordinary two-parameter atan
+# define  HYPOT               15  // get ordinary two-parameter hypot
+# define  GROUND_HEIGHT       16  // get
+# define  BUILD_PERCENT_LEFT  17  // get 0 = unit is built and ready, 1-100 = How much is left to build
+# define  YARD_OPEN           18  // set or get (change which plots we occupy when building opens and closes)
+# define  BUGGER_OFF          19  // set or get (ask other units to clear the area)
+# define  ARMORED             20  // set or get
+
+# define  MIN_ID                      69      // returns the lowest valid unit ID number
+# define  MAX_ID                      70      // returns the highest valid unit ID number
+# define  MY_ID                       71      // returns ID of current unit
+# define  UNIT_TEAM                   72      // returns team(player ID in TA) of unit given with parameter
+# define  UNIT_BUILD_PERCENT_LEFT     73      // basically BUILD_PERCENT_LEFT, but comes with a unit parameter
+# define  UNIT_ALLIED                 74      // is unit given with parameter allied to the unit of the current COB script. 0=not allied, not zero allied
+# define  UNIT_IS_ON_THIS_COMP        75      // indicates if the 1st parameter(a unit ID) is local to this computer
+# define  VETERAN_LEVEL               32      // gets kills * 100
+
+
+
+namespace TA3D
 {
-	int		VersionSignature;
-	int		NumberOfScripts;
-	int		NumberOfPieces;
-	int		Unknown_0;
-	int		Unknown_1;
-	int		Unknown_2; /* Always seems to be 0 */
-	int		OffsetToScriptCodeIndexArray;
-	int		OffsetToScriptNameOffsetArray;
-	int		OffsetToPieceNameOffsetArray;
-	int		OffsetToScriptCode;
-	int		Unknown_3; /* Always seems to point to first script name */
-};
 
-class SCRIPT			// Classe gérant le stockage et l'éxecution des scripts
-{
-public:
-	int		nb_script;		// Nombre de scripts
-	byte	*Data;			// Données du fichier COB
-	int		**script_code;	// Code des scripts
-	char	**name;			// Nom des scripts
-	int		nb_piece;		// Nombre de pièces
-	char	**piece_name;	// Nom des pièces de l'objet 3d concerné
-	int		*dec_offset;
+    class SCRIPT            // Classe gérant le stockage et l'éxecution des scripts
+    {
+    public:
+        SCRIPT() {init();}
+        ~SCRIPT() {destroy();}
 
-	void init()
-	{
-		Data=NULL;
-		nb_script=0;
-		nb_piece=0;
-		script_code=NULL;
-		name=NULL;
-		piece_name=NULL;
-		dec_offset=NULL;
-	}
+        void init();
+        void destroy();
 
-	SCRIPT()
-	{
-		init();
-	}
+        void load_cob(byte *data);
 
-	void destroy()
-	{
-		if(name) {
-			for(int i=0;i<nb_script;i++)
-				free(name[i]);
-			free(name);
-			}
-		if(script_code)
-			free(script_code);
-		if(dec_offset)
-			free(dec_offset);
-		if(Data)
-			delete[] Data;
-		if(piece_name) {
-			for(int i=0;i<nb_piece;i++)
-				free(piece_name[i]);
-			free(piece_name);
-			}
-		init();
-	}
+    public:
+        int     nb_script;      // Nombre de scripts
+        byte    *Data;          // Données du fichier COB
+        int     **script_code;  // Code des scripts
+        char    **name;         // Nom des scripts
+        int     nb_piece;       // Nombre de pièces
+        char    **piece_name;   // Nom des pièces de l'objet 3d concerné
+        int     *dec_offset;
 
-	~SCRIPT()
-	{
-		destroy();
-	}
+    }; // class SCRIPT
 
-	void load_cob(byte *data);
-};
 
-class SCRIPT_STACK			// Structure pour gérer la liste de la pile permettant l'éxecution des scripts
-{
-public:
-	int					val;			// Entier
-	SCRIPT_STACK		*next;			// pointeur vers l'élément suivant
+    /*!
+    ** \todo This class must be removed. A std::deque<int> or std::slits<int> will be good enough...
+    */
+    class SCRIPT_STACK          // Structure pour gérer la liste de la pile permettant l'éxecution des scripts
+    {
+    public:
+        SCRIPT_STACK() :val(0), next(NULL) {}
 
-	SCRIPT_STACK()
-	{
-		next=NULL;
-		val=0;
-	}
-};
+        int                 val;            // Entier
+        SCRIPT_STACK        *next;          // pointeur vers l'élément suivant
 
-	// la classe UNIT a été déplacée dans le fichier EngineClass.h
+    }; // class SCRIPT_STACK
+
+
+} // namespace TA3D
+
 
 #endif
