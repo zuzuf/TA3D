c06150c0ef0ebae273fad568b4c70c1cc8666339
Damien GERARD <milipili@users.noreply.github.com>
2008-07-31 14:46:35 +0000

    Added a new utility namespace : Converters, to convert PCX,3dm,obj files into something which can be handle easily Removed the global function load_memory_pcx().

--- src/ta3d.h
+++ src/ta3d.h
@@ -61,46 +61,6 @@ int cursor_type=CURSOR_DEFAULT;
 
 
 
-/*---------------------------------------------------------------------------------\
-|                              BITMAP *load_memory_pcx(...)                        |
-|        Charge le fichier pcx dont data pointe les données                        |
-\---------------------------------------------------------------------------------*/
-BITMAP *load_memory_pcx(byte *data,RGB *cpal)
-{
-	if(cpal==NULL) return NULL;		// On ne peut pas charger de fichier 8bits sans palette graphique
-	short width,height;
-	width=*((short*)(data+8));		// Dimensions
-	height=*((short*)(data+10));
-	width-=*((short*)(data+4));		// Dimensions
-	height-=*((short*)(data+6));
-
-	width++;
-	height++;
-
-	BITMAP *pcx = create_bitmap(width,height);
-	clear(pcx);
-
-	int pos=128;
-	for (int y = 0; y < pcx->h; ++y)
-    {
-		int x = 0;
-		do
-		{
-			int c=data[pos++];
-			if (c > 191)
-            {
-				int l=c-192;
-				c=data[pos++];
-				int col=makecol(cpal[c].r<<2,cpal[c].g<<2,cpal[c].b<<2);
-				for(;l>0 && x<pcx->w;l--)
-					((int*)(pcx->line[y]))[x++]=col;
-				}
-			else
-				((int*)(pcx->line[y]))[x++]=makecol(cpal[c].r<<2,cpal[c].g<<2,cpal[c].b<<2);
-		}while(x<pcx->w);
-		}
-	return pcx;
-}
 #endif
 
 
