fca3282a851584bba84c0a1e4b1d0c75ac94ff17
zuzuf <zuzuf@826f9f71-dfac-4a73-829b-727f66ff95d4>
2009-02-20 19:42:33 +0000

    * added LUA_ENV class, currently not bound with Lua threads, but it already has the required functions to manage a global Lua state

--- trunk/src/scripts/lua.env.cpp
+++ trunk/src/scripts/lua.env.cpp
@@ -14,3 +14,119 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
+
+#include "../stdafx.h"
+#include "lua.env.h"
+
+namespace TA3D
+{
+    LUA_ENV::LUA_ENV()
+    {
+        L = lua_open();
+    }
+
+    LUA_ENV::~LUA_ENV()
+    {
+        lua_close( L );
+    }
+
+    void LUA_ENV::set_global_string( const String &name, const String &value )
+    {
+        pMutex.lock();
+            lua_pushstring( L, value.c_str() );
+            lua_getglobal( L, name.c_str() );
+        pMutex.unlock();
+    }
+
+    void LUA_ENV::set_global_number( const String &name, const double value )
+    {
+        pMutex.lock();
+            lua_pushnumber( L, value );
+            lua_getglobal( L, name.c_str() );
+        pMutex.unlock();
+    }
+
+    void LUA_ENV::set_global_boolean( const String &name, const bool value )
+    {
+        pMutex.lock();
+            lua_pushboolean( L, value );
+            lua_getglobal( L, name.c_str() );
+        pMutex.unlock();
+    }
+
+    String LUA_ENV::get_global_string( const String &name )
+    {
+        pMutex.lock();
+            lua_getglobal( L, name.c_str() );
+            String result = lua_tostring( L, -1 ) ? String( lua_tostring( L, -1 ) ) : "";
+            lua_pop( L, 1 );
+        pMutex.unlock();
+        return result;
+    }
+
+    double LUA_ENV::get_global_number( const String &name )
+    {
+        pMutex.lock();
+            lua_getglobal( L, name.c_str() );
+            double result = lua_tonumber( L, -1 );
+            lua_pop( L, 1 );
+        pMutex.unlock();
+        return result;
+    }
+
+    bool LUA_ENV::get_global_boolean( const String &name )
+    {
+        pMutex.lock();
+            lua_getglobal( L, name.c_str() );
+            bool result = lua_toboolean( L, -1 );
+            lua_pop( L, 1 );
+        pMutex.unlock();
+        return result;
+    }
+
+    bool LUA_ENV::is_global_string( const String &name )
+    {
+        pMutex.lock();
+            lua_getglobal( L, name.c_str() );
+            bool result = lua_isstring( L, -1 );
+            lua_pop( L, 1 );
+        pMutex.unlock();
+        return result;
+    }
+
+    bool LUA_ENV::is_global_number( const String &name )
+    {
+        pMutex.lock();
+            lua_getglobal( L, name.c_str() );
+            bool result = lua_isnumber( L, -1 );
+            lua_pop( L, 1 );
+        pMutex.unlock();
+        return result;
+    }
+
+    bool LUA_ENV::is_global_boolean( const String &name )
+    {
+        pMutex.lock();
+            lua_getglobal( L, name.c_str() );
+            bool result = lua_isboolean( L, -1 );
+            lua_pop( L, 1 );
+        pMutex.unlock();
+        return result;
+    }
+
+    LUA_ENV *LUA_ENV::global = NULL;
+
+    LUA_ENV *LUA_ENV::instance()
+    {
+        if (global == NULL)
+            global = new LUA_ENV();
+        return global;
+    }
+
+    void LUA_ENV::destroy()
+    {
+        if (global)
+            delete global;
+        global = NULL;
+    }
+}
--- trunk/src/scripts/lua.env.h
+++ trunk/src/scripts/lua.env.h
@@ -14,3 +14,44 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
+
+#ifndef __LUA_ENV_H__
+#define __LUA_ENV_H__
+
+# include "../lua/lua.hpp"
+# include "../threads/thread.h"
+
+namespace TA3D
+{
+    /*!
+    ** This class manages the Lua global environment and ensures Lua threads
+    ** can access it safely
+    */
+    class LUA_ENV : public ObjectSync
+    {
+    protected:
+        lua_State   *L;             // The global Lua state
+    public:
+
+        LUA_ENV();
+        ~LUA_ENV();
+
+        void set_global_string( const String &name, const String &value );
+        void set_global_number( const String &name, const double value );
+        void set_global_boolean( const String &name, const bool value );
+        String get_global_string( const String &name );
+        double get_global_number( const String &name );
+        bool get_global_boolean( const String &name );
+        bool is_global_string( const String &name );
+        bool is_global_number( const String &name );
+        bool is_global_boolean( const String &name );
+
+    public:
+        static LUA_ENV *global;
+        static LUA_ENV *instance();
+        static void destroy();
+    };
+
+}
+
+#endif
