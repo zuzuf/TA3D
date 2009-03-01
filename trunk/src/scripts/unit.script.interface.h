126dcfb68cf995e7f09e8f8f83462de2ce0cfcda
zuzuf <zuzuf@826f9f71-dfac-4a73-829b-727f66ff95d4>
2009-03-01 13:30:13 +0000

    * scripts are now called by their ID, not their name, this will allow networking synchronization to use less bandwidth

--- trunk/src/scripts/unit.script.interface.h
+++ trunk/src/scripts/unit.script.interface.h
@@ -21,6 +21,56 @@
 # include "script.interface.h"
 # include "../misc/hash_table.h"
 
+#define SCRIPT_QueryPrimary		0x00
+#define SCRIPT_AimPrimary		0x01
+#define SCRIPT_FirePrimary		0x02
+#define SCRIPT_QuerySecondary	0x03
+#define SCRIPT_AimSecondary		0x04
+#define SCRIPT_FireSecondary	0x05
+#define SCRIPT_QueryTertiary	0x06
+#define SCRIPT_AimTertiary		0x07
+#define SCRIPT_FireTertiary		0x08
+#define SCRIPT_TargetCleared	0x09
+#define SCRIPT_stopbuilding		0x0A
+#define SCRIPT_stop				0x0B
+#define SCRIPT_startbuilding	0x0C
+#define SCRIPT_go				0x0D
+#define SCRIPT_killed			0x0E
+#define SCRIPT_StopMoving		0x0F
+#define SCRIPT_Deactivate		0x10
+#define SCRIPT_Activate			0x11
+#define SCRIPT_create			0x12
+#define SCRIPT_MotionControl	0x13
+#define SCRIPT_startmoving		0x14
+#define SCRIPT_MoveRate1		0x15
+#define SCRIPT_MoveRate2		0x16
+#define SCRIPT_MoveRate3		0x17
+#define SCRIPT_RequestState		0x18
+#define SCRIPT_TransportPickup	0x19
+#define SCRIPT_TransportDrop	0x1A
+#define SCRIPT_QueryTransport	0x1B
+#define SCRIPT_BeginTransport	0x1C
+#define SCRIPT_EndTransport		0x1D
+#define SCRIPT_SetSpeed			0x1E
+#define SCRIPT_SetDirection		0x1F
+#define SCRIPT_SetMaxReloadTime	0x20
+#define SCRIPT_QueryBuildInfo	0x21
+#define SCRIPT_SweetSpot		0x22
+#define SCRIPT_RockUnit			0x23
+#define SCRIPT_QueryLandingPad	0x24
+#define SCRIPT_setSFXoccupy     0x25
+#define SCRIPT_HitByWeapon      0x26
+#define SCRIPT_QueryNanoPiece   0x27
+#define SCRIPT_AimFromPrimary   0x28
+#define SCRIPT_AimFromSecondary 0x29
+#define SCRIPT_AimFromTertiary  0x2A
+#define NB_SCRIPT				0x2B
+
+#define SCRIPT_QueryWeapon      NB_SCRIPT
+#define SCRIPT_AimWeapon        (NB_SCRIPT + 1)
+#define SCRIPT_AimFromWeapon    (NB_SCRIPT + 2)
+#define SCRIPT_FireWeapon       (NB_SCRIPT + 3)
+
 namespace TA3D
 {
     /*!
@@ -40,8 +90,12 @@ namespace TA3D
         int getReturnValue(const String &name);
         void setReturnValue(const String &name, int value);
 
+    private:
+        static const char *script_name[];
     public:
         static UNIT_SCRIPT_INTERFACE *instanciate( SCRIPT_DATA *data );
+        static String get_script_name(int id);
+        static int get_script_id(const String &name);
     };
 }
 
