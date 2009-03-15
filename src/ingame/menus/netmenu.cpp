5e70ac5f96091400ea0b42d0a226b48a15d99058
zuzuf <zuzuf@users.noreply.github.com>
2009-03-15 21:20:33 +0000

    * improving the NetMenu object * added gui descriptors for NetMenu * you can log in/out and register from the GUI * GUI displays peer list

--- src/ingame/menus/netmenu.cpp
+++ src/ingame/menus/netmenu.cpp
@@ -20,7 +20,6 @@ namespace TA3D
 
         NetMenu::~NetMenu()
         {
-            NetClient::instance()->disconnect();
         }
 
 
@@ -70,6 +69,39 @@ namespace TA3D
         bool NetMenu::maySwitchToAnotherMenu()
         {
             // First we have to parse the server messages
+            while(NetClient::instance()->messageWaiting() && !pArea->get_state("popup"))
+            {
+                String msg = NetClient::instance()->getNextMessage();
+                if (StartsWith(msg, "MESSAGE"))
+                {
+                    pArea->set_title("popup", I18N::Translate("Server message"));
+                    pArea->set_caption("popup.msg", I18N::Translate(msg.substr(8)));
+                    pArea->msg("popup.show");
+                }
+                else if (StartsWith(msg, "ERROR"))
+                {
+                    pArea->set_title("popup", I18N::Translate("Server error"));
+                    pArea->set_caption("popup.msg", I18N::Translate(msg.substr(6)));
+                    pArea->msg("popup.show");
+                }
+            }
+
+            pArea->set_entry("netmenu.peer_list", NetClient::instance()->getPeerList());
+
+            pArea->set_enable_flag("netmenu.b_login", !(netMode == LOGIN || NetClient::instance()->getState() == NetClient::CONNECTED));
+            pArea->set_enable_flag("netmenu.b_logout", NetClient::instance()->getState() == NetClient::CONNECTED);
+            pArea->set_enable_flag("netmenu.b_register", NetClient::instance()->getState() != NetClient::CONNECTED);
+
+            if (NetClient::instance()->getState() == NetClient::CONNECTED)
+            {
+                pArea->msg("netmenu.b_login.hide");
+                pArea->msg("netmenu.b_logout.show");
+            }
+            else
+            {
+                pArea->msg("netmenu.b_login.show");
+                pArea->msg("netmenu.b_logout.hide");
+            }
 
             // Now we run all processes to get logged/registered
             switch(netMode)
@@ -94,7 +126,9 @@ namespace TA3D
                     }
                     else if (NetClient::instance()->getState() != NetClient::CONNECTED)
                     {
+
                         NetClient::instance()->connect(lp_CONFIG->net_server, 4240, login, password);
+                        netMode = NONE;
                     }
                     break;
                 case LOGIN:
@@ -107,6 +141,7 @@ namespace TA3D
                     {
                         login.clear();
                         askMode = NONE;
+                        netMode = NONE;
                     }
                     break;
                 case PASSWORD:
@@ -120,6 +155,7 @@ namespace TA3D
                         login.clear();
                         password.clear();
                         askMode = NONE;
+                        netMode = NONE;
                     }
                     break;
                 };
@@ -145,6 +181,7 @@ namespace TA3D
                     else if (NetClient::instance()->getState() != NetClient::CONNECTED)
                     {
                         NetClient::instance()->connect(lp_CONFIG->net_server, 4240, login, password, true);
+                        netMode = NONE;
                     }
                     break;
                 case LOGIN:
@@ -157,6 +194,7 @@ namespace TA3D
                     {
                         login.clear();
                         askMode = NONE;
+                        netMode = NONE;
                     }
                     break;
                 case PASSWORD:
@@ -170,12 +208,29 @@ namespace TA3D
                         login.clear();
                         password.clear();
                         askMode = NONE;
+                        netMode = NONE;
                     }
                     break;
                 };
                 break;
             case NONE:
             default:
+                login.clear();
+                password.clear();
+                if (pArea->get_state("netmenu.b_register"))
+                {
+                    netMode = REGISTER;
+                    askMode = NONE;
+                }
+                else if (pArea->get_state("netmenu.b_login"))
+                {
+                    netMode = LOGIN;
+                    askMode = NONE;
+                }
+                else if (pArea->get_state("netmenu.b_logout"))
+                {
+                    NetClient::instance()->destroyInstance();
+                }
                 break;
             };
 
