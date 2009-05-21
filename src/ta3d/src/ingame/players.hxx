746ec67c4139966fcd482de810078c9436e87bd7
Damien GERARD <milipili@users.noreply.github.com>
2009-05-21 11:39:30 +0000

    Added missing file

---
+++ src/ta3d/src/ingame/players.hxx
@@ -0,0 +1,19 @@
+#ifndef __TA3D_INGAME_PLAYERS_HXX__
+# define __TA3D_INGAME_PLAYERS_HXX__
+
+
+
+namespace TA3D
+{
+
+
+	inline unsigned int PLAYERS::count() const
+	{
+		return pPlayerCount;
+	}
+
+
+
+} // namespace TA3D
+
+#endif // __TA3D_INGAME_PLAYERS_H__
