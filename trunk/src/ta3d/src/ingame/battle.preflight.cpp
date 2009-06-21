274b92581cb9ae367e77a07a6d98cc1587132842
zuzuf <zuzuf@826f9f71-dfac-4a73-829b-727f66ff95d4>
2009-06-21 22:13:50 +0000

    * fixed zoom with orthographic camera

--- trunk/src/ta3d/src/ingame/battle.preflight.cpp
+++ trunk/src/ta3d/src/ingame/battle.preflight.cpp
@@ -117,12 +117,23 @@ namespace TA3D
 	void Battle::preflightAutomaticCamera()
 	{
 		float old_zscroll = camera_zscroll;
-		int delta = IsOnGUI ? 0 : mouse_z -omz;
-		camera_zscroll += delta * 2.0f * lp_CONFIG->camera_zoom_speed;
-		if (camera_zscroll < -25.0f)
-			camera_zscroll = -25.0f;
-		else
-			if (camera_zscroll > 20.0f) camera_zscroll = 20.0f;
+        int delta = IsOnGUI ? 0 : mouse_z - omz;
+        if (lp_CONFIG->ortho_camera)        // 2D zoom with orthographic camera
+        {
+            camera_zscroll += delta * lp_CONFIG->camera_zoom_speed;
+            if (camera_zscroll < -50.0f)
+                camera_zscroll = -50.0f;
+            else
+                if (camera_zscroll > 50.0f) camera_zscroll = 50.0f;
+        }
+        else
+        {
+            camera_zscroll += delta * 2.0f * lp_CONFIG->camera_zoom_speed;
+            if (camera_zscroll < -25.0f)
+                camera_zscroll = -25.0f;
+            else
+                if (camera_zscroll > 20.0f) camera_zscroll = 20.0f;
+        }
 
 		if ((msec_timer - cam_def_timer) * Conv >= 1.0f && delta != 0
 			&& ( ( camera_zscroll > 0.0f && old_zscroll <= 0.0f)
@@ -144,8 +155,26 @@ namespace TA3D
 			r1 = -lp_CONFIG->camera_def_angle;      // angle is constant
 			if (r1 > -45.0f) 		r1 = -45.0f;
 			else if (r1 < -90.0f)	r1 = -90.0f;
-			cam.zoomFactor = 0.5f * expf(-camera_zscroll * 0.05f * logf(Math::Max(map->map_w / SCREEN_W, map->map_h / SCREEN_H)));
-			cam_h = lp_CONFIG->camera_def_h * 2.0f * cam.zoomFactor;
+            float zoom_min = Math::Max(((float)map->map_w) / SCREEN_W, ((float)map->map_h) / SCREEN_H);     // ==> x2
+            float zoom_max = 0.02f;                                                                          // ==> x0
+            float zoom_med = 0.5f;
+            float x0 = -50.0f;
+            float x2 = 50.0f;
+            float sm = 0.01f;
+            float x1 = x0 + log((zoom_med - zoom_min) / (zoom_max - zoom_min) * (exp(sm * (x2 - x0)) - 1.0f) + 1.0f) / sm;
+//			cam.zoomFactor = 0.5f * expf(-camera_zscroll * 0.05f * logf(Math::Max(map->map_w / SCREEN_W, map->map_h / SCREEN_H)));
+            float x = camera_zscroll;
+            float xt = (x2 + x0) * 0.5f;
+            float z(0.0f);
+            if (x <= xt)
+                z = x0 + (x1 - x0) * (x - x0) / (xt - x0);
+            else
+                z = x1 + (x2 - x1) * (x - xt) / (x2 - xt);
+//            z = x;//Math::Max(Math::Min(x2, xt + x), x0);
+//            = x0 + (x1 - x0) * (x - x0) * (x - x2) / ((x1 - x0) * (x1 - x2))
+//                               + (x2 - x0) * (x - x0) * (x - x1) / ((x2 - x0) * (x2 - x1));
+            cam.zoomFactor = zoom_min + (zoom_max - zoom_min) * (exp(sm * (z - x0)) - 1.0f) / (exp(sm * (x2 - x0)) - 1.0f);
+            cam_h = lp_CONFIG->camera_def_h * 2.0f * cam.zoomFactor;
 		}
 		else                                // Mega zoom with a perspective camera
 		{
