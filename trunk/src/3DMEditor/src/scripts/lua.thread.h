f99a9a03cf2ebf7b5954074f2f65013b681c9218
zuzuf <zuzuf@826f9f71-dfac-4a73-829b-727f66ff95d4>
2009-08-05 19:19:56 +0000

    * added several code loaders in various modules

--- trunk/src/3DMEditor/src/scripts/lua.thread.h
+++ trunk/src/3DMEditor/src/scripts/lua.thread.h
@@ -35,73 +35,9 @@ namespace TA3D
     /*!
     ** \brief loads a Lua script, preprocessing to allow including other files and use defines
     */
-    byte *loadLuaFile(const QString &filename, uint32 &filesize);
+    byte *loadLuaCode(const QString &code, uint32 &filesize);
 
-    /*!
-    ** This class represents a basic Lua thread without specialization
-    ** To use it, create a new class that inherits LuaThread
-    */
-    class LuaThread : public ScriptInterface
-    {
-        virtual const char *className() { return "LuaThread"; }
-        friend class UnitScript;
-    protected:
-        byte        *buffer;
-        lua_State   *L;             // The Lua state
-        int         n_args;         // Number of arguments given to lua_resume
-
-        QString     name;
-
-        bool        crashed;
-
-        int         nextID;
-
-    public:
-
-        LuaThread();
-        virtual ~LuaThread();
-
-        int getMem();
-
-        inline int getNextID()
-        {
-            if (caller)
-                return static_cast<LuaThread*>(caller)->getNextID();
-            return nextID++;
-        }
-
-        void init();
-        void destroy();
-
-        void load(const QString &filename);                    // Load a lua script
-
-        virtual int run(float dt, bool alone = false);      // Run the script
-        int run();                          // Run the script with default delay
-
-        //! functions used to call/run Lua functions
-        void call(const QString &functionName, int *parameters = NULL, int nb_params = 0);
-        int execute(const QString &functionName, int *parameters = NULL, int nb_params = 0);
-
-        //! functions used to create new threads sharing the same environment
-        virtual LuaThread *fork();
-        virtual LuaThread *fork(const QString &functionName, int *parameters = NULL, int nb_params = 0);
-        virtual LuaThread *fork(lua_State *cL, int n);
-
-        //! functions used for debugging
-        inline bool is_crashed()    {   return crashed;  }
-        inline void crash()         {   crashed = true;  }
-        inline void uncrash()       {   crashed = false; }
-        bool runCommand(const QString &cmd);
-    private:
-        //! functions to manipulate the Lua processes
-        void setThreadID();
-
-    private:
-        //! functions that register new Lua functions
-        void register_basic_functions();
-        virtual void register_functions()   {}
-        virtual void register_info()        {}
-    };
+    byte *readFile(const QString &filename, uint32 *filesize);
 
     int thread_logmsg( lua_State *L );
     int thread_mouse_x( lua_State *L );
@@ -109,13 +45,6 @@ namespace TA3D
     int thread_mouse_z( lua_State *L );
     int thread_mouse_b( lua_State *L );
     int thread_time( lua_State *L );
-    int thread_signal( lua_State *L );
-    int thread_start_script( lua_State *L );
-
-    /*!
-    ** \brief returns a pointer to the current thread, or NULL on error
-    */
-    LuaThread *lua_threadID( lua_State *L );
 }
 
 #endif
