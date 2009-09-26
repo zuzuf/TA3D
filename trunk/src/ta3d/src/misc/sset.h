af6d79091d36a80f23c5b5498fb7827f75c20fc4
milipili <milipili@826f9f71-dfac-4a73-829b-727f66ff95d4>
2009-09-26 17:15:46 +0000

    Minor improvments for the sset

--- trunk/src/ta3d/src/misc/sset.h
+++ trunk/src/ta3d/src/misc/sset.h
@@ -3,80 +3,95 @@
 
 #include "slist.h"
 
+
+
 namespace TA3D
 {
 
+
 	template< typename T, typename Container = slist<T> >
-			class sset
+	class sset
 	{
-	private:
-		Container data;
 	public:
 		typedef typename Container::iterator	iterator;
 		typedef const iterator const_iterator;
 
-		inline sset() : data()	{}
+	public:
+		sset() : data()	{}
 
-		inline sset(const sset<T> &x)
+		sset(const sset<T> &x)
 		{
 			data = x.data;
 		}
 
-		inline sset<T> &operator=( const sset<T> &x )
+		sset<T> &operator= (const sset<T> &x)
 		{
 			clear();
 			data = x.data;
 			return *this;
 		}
 
-		inline iterator begin() {   return data.begin();    }
-		inline iterator end()   {   return data.end();    }
-		inline const_iterator begin() const {   return data.begin();    }
-		inline const_iterator end()   const {   return data.end();    }
+		iterator begin() {   return data.begin();    }
+		iterator end()   {   return data.end();    }
+		const_iterator begin() const {   return data.begin();    }
+		const_iterator end()   const {   return data.end();    }
 
-		inline void clear()			{	data.clear();	}
+		void clear()			{	data.clear();	}
 
-		inline bool empty() const   {   return data.empty();  }
-		inline int size() const     {   return data.size();   }
+		bool empty() const   {   return data.empty();  }
+		int size() const     {   return data.size();   }
 
-		inline void erase(iterator i)
+		void erase(iterator i)
 		{
 			data.erase(i);
 		}
 
-		inline void add(const T &value)
+		void add(const T &value)
 		{
 			data.push_front(value);
 		}
 
-		inline void isIn(const T &value) const
+		void isIn(const T &value) const
 		{
-			for(const_iterator i = begin() ; i != end() ; ++i)
+			const const_iterator end = this->end();
+			for (const_iterator i = begin(); i != end; ++i)
+			{
 				if (*i == value)
 					return true;
+			}
 			return false;
 		}
 
-		inline void remove(const T &value)
+		void remove(const T &value)
 		{
-			for(iterator i = begin() ; i != end() ; ++i)
+			const iterator end = this->end();
+			for (iterator i = begin(); i != end; ++i)
+			{
 				if (*i == value)
 				{
 					data.erase(i);
 					return;
 				}
+			}
 		}
 
-		inline void swap(sset<T> &x)
+		void swap(sset<T> &x)
 		{
 			data.swap(x.data);
 		}
 
-		inline Container &getData()
+		Container &getData()
 		{
 			return data;
 		}
-	};
-}
+
+	private:
+		Container data;
+
+	}; // class sset
+
+
+
+} // namespace TA3D
 
 #endif
