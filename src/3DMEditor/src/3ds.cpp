4c8520ce6342d820d8d71c4f6f99d92dead8d9bf
zuzuf <zuzuf@users.noreply.github.com>
2010-01-09 17:15:54 +0000

    * fixed 3DS loader not supporting unicode filenames (FILE* based code rewritten using QFile)

--- src/3DMEditor/src/3ds.cpp
+++ src/3DMEditor/src/3ds.cpp
@@ -52,82 +52,87 @@ public:
     }
 };
 
-QString read_ASCIIZ( FILE *src)
+QString read_ASCIIZ( QFile &src)
 {
     char name[1000];
     int i = 0;
-    int c = 0;
-    while ((c = fgetc(src)))
+	unsigned char c = 0;
+	while (src.getChar((char*)&c) && c != 0)
         name[ i++ ] = c;
     name[ i ] = 0;
     return QString(name);
 }
 
-void read_color_chunk( float color[], FILE *src )
+void read_color_chunk( float color[], QFile &src )
 {
     TA3D_3DS_CHUNK_DATA chunk;
-    fread( &chunk.ID, 2, 1, src );
-    fread( &chunk.length, 4, 1, src );
+	src.read( (char*)&chunk.ID, 2 );
+	src.read( (char*)&chunk.length, 4 );
     switch( chunk.ID )
     {
     case COL_RGB:
     case COL_RGB2:
-        fread( color, sizeof( float ), 3, src );
+		src.read( (char*)color, sizeof( float ) * 3 );
         break;
     case COL_TRU:
     case COL_TRU2:
         for( int i = 0 ; i < 3 ;i++ )
-            color[ i ] = fgetc( src ) / 255.0f;
+		{
+			unsigned char c;
+			src.getChar((char*)&c);
+			color[ i ] = c / 255.0f;
+		}
         break;
     default:
-        fseek( src, chunk.length - 6, SEEK_CUR );
+		src.read( chunk.length - 6 );
     };
 }
 
-float read_percent_chunk( FILE *src )
+float read_percent_chunk( QFile &src )
 {
     TA3D_3DS_CHUNK_DATA chunk;
-    fread( &chunk.ID, 2, 1, src );
-    fread( &chunk.length, 4, 1, src );
+	src.read( (char*)&chunk.ID, 2 );
+	src.read( (char*)&chunk.length, 4 );
     switch( chunk.ID )
     {
     case PER_INT:
         {
             uint16	percent;
-            fread( &percent, 2, 1, src );
+			src.read( (char*)&percent, 2 );
             return percent;
         }
     case PER_FLOAT:
         {
             float	percent;
-            fread( &percent, 4, 1, src );
+			src.read( (char*)&percent, 4 );
             return percent;
         }
     default:
-        fseek( src, chunk.length - 6, SEEK_CUR );
+		src.read( chunk.length - 6 );
     };
     return 0.0f;
 }
 
-QString read_MAT_MAPNAME_chunk( FILE *src )
+QString read_MAT_MAPNAME_chunk( QFile &src )
 {
     TA3D_3DS_CHUNK_DATA chunk;
-    fread( &chunk.ID, 2, 1, src );
-    fread( &chunk.length, 4, 1, src );
+	src.read( (char*)&chunk.ID, 2 );
+	src.read( (char*)&chunk.length, 4 );
     switch( chunk.ID )
     {
     case MAT_MAPNAME:
         return read_ASCIIZ( src );
     default:
-        fseek( src, chunk.length - 6, SEEK_CUR );
+		src.read( chunk.length - 6 );
     };
     return NULL;
 }
 
 void Mesh::load3DS(const QString &filename, float scale)
 {
-    FILE *src_3ds = fopen( filename.toStdString().c_str(), "rb" );
-    if (src_3ds)
+	QFile src_3ds(filename);
+	src_3ds.open(QFile::ReadOnly);
+	if (src_3ds.isOpen())
     {
         destroy();
         QString filepath = filename;
@@ -140,10 +145,11 @@ void Mesh::load3DS(const QString &filename, float scale)
         Mesh *read_obj = NULL;
         TA3D_3DS_MATERIAL	*material = NULL;
         Vector3D local[4];
-        while( fread( &chunk.ID, sizeof( chunk.ID ), 1, src_3ds ) )
+		while (src_3ds.read( (char*)&chunk.ID, sizeof( chunk.ID ) ))
         {
-            if( fread( &chunk.length, sizeof( chunk.length ), 1, src_3ds ) == 0 )	break;
-            switch( chunk.ID )
+			if (src_3ds.read( (char*)&chunk.length, sizeof( chunk.length ) ) == 0)
+				break;
+			switch (chunk.ID)
             {
             case MAIN3DS:
                 //				printf("MAIN3DS (%d,%d)\n", chunk.ID, chunk.length);
@@ -164,72 +170,71 @@ void Mesh::load3DS(const QString &filename, float scale)
                 {
                     QString name = read_ASCIIZ( src_3ds );
                     //								printf( "name = %s\n", name );
-                    if( material )
+					if (material)
                         material->NAME = name;
                 }
                 break;
             case MAT_AMBIENT:
                 //							printf("---MAT_AMBIENT (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     read_color_chunk( material->AMBIENT, src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_DIFFUSE:
                 //							printf("---MAT_DIFFUSE (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     read_color_chunk( material->DIFFUSE, src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_SPECULAR:
                 //							printf("---MAT_SPECULAR (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     read_color_chunk( material->SPECULAR, src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_SHININESS:
                 //							printf("---MAT_SHININESS (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     material->SHININESS = read_percent_chunk( src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_SHIN2PCT:
                 //							printf("---MAT_SHIN2PCT (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     material->SHIN2PCT = read_percent_chunk( src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_SHIN3PCT:
                 //							printf("---MAT_SHIN3PCT (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     material->SHIN3PCT = read_percent_chunk( src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_TRANSPARENCY:
                 //							printf("---MAT_TRANSPARENCY (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     material->TRANSPARENCY = read_percent_chunk( src_3ds );
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_TWO_SIDE:
                 //							printf("---MAT_TWO_SIDE (%d,%d)\n", chunk.ID, chunk.length);
-                if( material )
+				if (material)
                     material->TWO_SIDE = true;
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case MAT_TEXMAP:
                 //							printf("---MAT_TEXMAP (%d,%d)\n", chunk.ID, chunk.length);
                 if (material)
                 {
                     uint16 n_id;
-                    fread( &n_id, 2, 1, src_3ds );
-                    fseek( src_3ds, -2, SEEK_CUR );
+					src_3ds.peek( (char*)&n_id, 2 );
                     if (n_id == MAT_MAPNAME)
                     {
                         material->MAPNAME = read_MAT_MAPNAME_chunk( src_3ds );
@@ -243,39 +248,39 @@ void Mesh::load3DS(const QString &filename, float scale)
                     material->MAPNAME = filepath + material->MAPNAME;
                 }
                 else
-                    fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+					src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_CONFIG1:
                 //						printf("--EDIT_CONFIG1 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_CONFIG2:
                 //						printf("--EDIT_CONFIG2 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_VIEW_P1:
                 //						printf("--EDIT_VIEW_P1 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_VIEW_P2:
                 //						printf("--EDIT_VIEW_P2 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_VIEW_P3:
                 //						printf("--EDIT_VIEW_P2 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_VIEW1:
                 //						printf("--EDIT_VIEW1 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_BACKGR:
                 //						printf("--EDIT_BACKGR (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_AMBIENT:
                 //						printf("--EDIT_AMBIENT (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case EDIT_OBJECT:
                 //						printf("--EDIT_OBJECT (%d,%d)\n", chunk.ID, chunk.length);
@@ -293,19 +298,19 @@ void Mesh::load3DS(const QString &filename, float scale)
                 break;
             case OBJ_LIGHT:
                 //							printf("---OBJ_LIGHT (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case OBJ_CAMERA:
                 //							printf("---OBJ_CAMERA (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case OBJ_UNKNWN01:
                 //							printf("---OBJ_UNKNWN01 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case OBJ_UNKNWN02:
                 //							printf("---OBJ_UNKNWN02 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case OBJ_TRIMESH:
                 //							printf("---OBJ_TRIMESH (%d,%d)\n", chunk.ID, chunk.length);
@@ -325,7 +330,7 @@ void Mesh::load3DS(const QString &filename, float scale)
                 {
                     //								printf("----TRI_VERTEXL (%d,%d)\n", chunk.ID, chunk.length);
                     uint16 nb_vtx;
-                    fread( &nb_vtx, 2, 1, src_3ds );
+					src_3ds.read( (char*)&nb_vtx, 2 );
                     read_obj->vertex.resize( nb_vtx );
                     read_obj->normal.resize( nb_vtx );
                     if (read_obj->tcoord.isEmpty())
@@ -337,7 +342,7 @@ void Mesh::load3DS(const QString &filename, float scale)
                             read_obj->tcoord[ (i << 1) + 1 ] = 0.0f;
                         }
                     }
-                    fread( read_obj->vertex.data(), sizeof( Vector3D ), nb_vtx, src_3ds );
+					src_3ds.read( (char*)read_obj->vertex.data(), sizeof( Vector3D ) * nb_vtx );
                     for( int i = 0 ; i < read_obj->vertex.size() ; i++ )
                     {
                         read_obj->vertex[ i ] = scale * (read_obj->vertex[ i ].x * local[ 0 ] + read_obj->vertex[ i ].y * local[ 1 ] + read_obj->vertex[ i ].z * local[ 2 ] + local[ 3 ]);
@@ -347,7 +352,7 @@ void Mesh::load3DS(const QString &filename, float scale)
                 break;
             case TRI_FACEL2:
                 //								printf("----TRI_FACEL2 (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case TRI_MATERIAL:
                 //								printf("----TRI_MATERIAL (%d,%d)\n", chunk.ID, chunk.length);
@@ -378,11 +383,11 @@ void Mesh::load3DS(const QString &filename, float scale)
                         printf("WARNING: material not found!!\n");
 
                     uint16	nb_faces;
-                    fread( &nb_faces, 2, 1, src_3ds );
+					src_3ds.read( (char*)&nb_faces, 2 );
                     for( int i = 0 ; i < nb_faces ; i++ )
                     {
                         uint16 cur_face;
-                        fread( &cur_face, 2, 1, src_3ds );
+						src_3ds.read( (char*)&cur_face, 2 );
                     }
                 }
                 break;
@@ -390,9 +395,9 @@ void Mesh::load3DS(const QString &filename, float scale)
                 //								printf("----TRI_MAPPING (%d,%d)\n", chunk.ID, chunk.length);
                 {
                     uint16	nb_vtx;
-                    fread( &nb_vtx, 2, 1, src_3ds );
+					src_3ds.read( (char*)&nb_vtx, 2 );
                     read_obj->tcoord.resize(2 * nb_vtx);
-                    fread( read_obj->tcoord.data(), 2 * sizeof( float ), nb_vtx, src_3ds );
+					src_3ds.read( (char*)read_obj->tcoord.data(), 2 * sizeof( float ) * nb_vtx );
                     for( int i = 0 ; i < nb_vtx ; i++ )
                         read_obj->tcoord[ i * 2 + 1 ] = 1.0f - read_obj->tcoord[ i * 2 + 1 ];
                 }
@@ -400,12 +405,12 @@ void Mesh::load3DS(const QString &filename, float scale)
             case TRI_FACEL1:
                 //								printf("----TRI_FACEL1 (%d,%d)\n", chunk.ID, chunk.length);
                 uint16 nb_index;
-                fread( &nb_index, 2, 1, src_3ds );
+				src_3ds.read( (char*)&nb_index, 2 );
                 read_obj->index.resize(nb_index * 3);
                 for( int i = 0 ; i < nb_index * 3 ; i += 3 )
                 {
                     uint16 idx[3];
-                    fread( idx, 2, 3, src_3ds );
+					src_3ds.read( (char*)idx, 2 * 3 );
                     read_obj->index[i] = idx[0];
                     read_obj->index[i+1] = idx[1];
                     read_obj->index[i+2] = idx[2];
@@ -421,7 +426,7 @@ void Mesh::load3DS(const QString &filename, float scale)
                         read_obj->normal[ read_obj->index[ i + 2 ] ] = read_obj->normal[ read_obj->index[ i + 2 ] ] + AB;
                     }
                     uint16 face_info;
-                    fread( &face_info, 2, 1, src_3ds );
+					src_3ds.read( (char*)&face_info, 2 );
                 }
                 if (!read_obj->vertex.isEmpty())
                     for( int i = 0 ; i < read_obj->normal.size() ; i++ )
@@ -429,27 +434,27 @@ void Mesh::load3DS(const QString &filename, float scale)
                 break;
             case TRI_SMOOTH:
                 //								printf("----TRI_SMOOTH (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             case TRI_LOCAL:
                 //								printf("----TRI_LOCAL (%d,%d)\n", chunk.ID, chunk.length);
-                fread( &(local[0]), sizeof( Vector3D ), 1, src_3ds );		// X
-                fread( &(local[1]), sizeof( Vector3D ), 1, src_3ds );		// Y
-                fread( &(local[2]), sizeof( Vector3D ), 1, src_3ds );		// Z
-                fread( &(local[3]), sizeof( Vector3D ), 1, src_3ds );		// local origin
+				src_3ds.read( (char*)&(local[0]), sizeof( Vector3D ) );		// X
+				src_3ds.read( (char*)&(local[1]), sizeof( Vector3D ) );		// Y
+				src_3ds.read( (char*)&(local[2]), sizeof( Vector3D ) );		// Z
+				src_3ds.read( (char*)&(local[3]), sizeof( Vector3D ) );		// local origin
                 break;
             case TRI_VISIBLE:
                 //								printf("----TRI_VISIBLE (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
                 break;
             default:
                 //				printf("unknown (%d,%d)\n", chunk.ID, chunk.length);
-                fseek( src_3ds, chunk.length - 6, SEEK_CUR );
+				src_3ds.read( chunk.length - 6 );
             }
         }
         if (material)
             delete material;
-        fclose( src_3ds );
+		src_3ds.close();
 
         computeInfo();
         emit loaded();
