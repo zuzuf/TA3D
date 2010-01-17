22b52313c976f2d971ef8e3966aebedecd8268f5
zuzuf <zuzuf@users.noreply.github.com>
2010-01-17 15:30:05 +0000

    * Synchronizer rewritten using code from Yuni::Thread::Condition with pSignalled being an unsigned int instead of a bool

--- src/ta3d/src/threads/mutex.cpp
+++ src/ta3d/src/threads/mutex.cpp
@@ -4,41 +4,61 @@ namespace TA3D
 {
 	Synchronizer::Synchronizer(int nbThreadsToSync)
 			: nbThreadsToSync(nbThreadsToSync),
-			nbThreadsWaiting(0)
+			nbThreadsWaiting(0),
+			pSignalled(0)
 	{
+		// Note: The linux implementation currently does not support
+		// any condition attr
+		::pthread_cond_init(&pCondition, NULL);
 	}
 
 	Synchronizer::~Synchronizer()
 	{
 		nbThreadsToSync = 0;
 		release();
+		::pthread_cond_destroy(&pCondition);
 	}
 
 	void Synchronizer::sync()
 	{
-		lock();
+		pMutex.lock();
 		++nbThreadsWaiting;
 
 		if (nbThreadsToSync <= nbThreadsWaiting)
 		{
 			nbThreadsWaiting = 0;
-			notifyAll();
-			unlock();
+			++pSignalled;
+			::pthread_cond_broadcast(&pCondition);
+			pMutex.unlock();
 		}
 		else
 		{
-			waitUnlocked();
-			unlock();
+			// The pthread_cond_wait will unlock the mutex and wait for
+			// signalling.
+
+			int curSignal = pSignalled;
+			int pthread_cond_wait_error;
+			do
+			{
+				// Spurious wakeups from this function can occur.
+				// Therefore we must check out pSignalled variable to ensure we have
+				// really been signalled.
+				pthread_cond_wait_error = ::pthread_cond_wait(&pCondition, &pMutex.pthreadMutex());
+			} while (pSignalled == curSignal);
+
+			pMutex.unlock();
 		}
 	}
 
 	void Synchronizer::release()
 	{
-		lock();
+		pMutex.lock();
 
 		nbThreadsWaiting = 0;
-		notifyAll();
+		++pSignalled;
 
-		unlock();
+		::pthread_cond_broadcast(&pCondition);
+
+		pMutex.unlock();
 	}
 }
