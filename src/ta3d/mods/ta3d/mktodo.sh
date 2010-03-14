1bec54aa68014459bf5e09c1b06eafef0ca8b0d2
zuzuf <zuzuf@users.noreply.github.com>
2010-03-14 17:30:54 +0000

    * updated mktodo.sh script * updated TODO file

--- src/ta3d/mods/ta3d/mktodo.sh
+++ src/ta3d/mods/ta3d/mktodo.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 echo "files to create:" > TODO
-for i in `cat files.txt | tr " " "²"`; do test -e "`echo $i | tr "²" " "`" || echo $i >> TODO; done  
+for i in `cat files.txt | tr " " "²"`; do
+	test -e "`echo $i | tr "²" " "`" || test -e "`echo $i | tr "²" " " | sed -e s/\.3do$/\.s3o/`" || test -e "`echo $i | tr "²" " " | sed -e s/\.gaf$//`" || test -e "`echo $i | tr "²" " " | sed -e s/\.cob$/\.lua/`" || echo $i >> TODO;
+done
