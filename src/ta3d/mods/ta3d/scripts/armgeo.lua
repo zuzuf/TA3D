0cbf0dad74114ea5cb3e2b2fc0fa6204017f1f09
zuzuf <zuzuf@users.noreply.github.com>
2010-03-27 15:05:30 +0000

    * new model for ARMGEO (Cremuss's model with home made texture) * ARMGEO script updated to animate our new model (there is a fan and it emits smoke from 2 parts of the model)

--- src/ta3d/mods/ta3d/scripts/armgeo.lua
+++ src/ta3d/mods/ta3d/scripts/armgeo.lua
@@ -1,15 +1,31 @@
 -- Arm Geo Power Plant Script
 createUnitScript("armgeo")
 
-__this:piece("base", "smoke", "pieces")
+__this:piece("base", "fan", "smoke", "smoke2")
 
 __this.SMOKEPIECE1 = __this.base
 
 #include "exptype.lh"
 #include "smokeunit.lh"
 
+__this.SmokeEmission = function(this)
+    while true do
+        this:emit_sfx( SFXTYPE_WHITESMOKE, this.smoke )
+        this:emit_sfx( SFXTYPE_WHITESMOKE, this.smoke2 )
+        this:sleep( 0.1 )
+    end
+end
+
 __this.Create = function(this)
+    -- Wait until the unit is actually built
+    this:turn_piece_now( this.fan, x_axis, 180 )
+    this:move_piece_now( this.fan, y_axis, 2.0 )
+    while this:get(BUILD_PERCENT_LEFT) > 0 do
+        sleep(0.4)
+    end
+    this:spin( this.fan, y_axis, 90, 90 )
 	this:start_script( this.SmokeUnit, this )
+    this:start_script( this.SmokeEmission, this )
 end
 
 __this.SweetSpot = function(this)
@@ -19,28 +35,24 @@ end
 __this.Killed = function( this, severity )
 	if severity <= 25 then
 		this:explode( this.base, BITMAPONLY + BITMAP1 )
-		this:explode( this.smoke, BITMAPONLY + BITMAP2 )
-		this:explode( this.pieces, BITMAPONLY + BITMAP2 )
+		this:explode( this.fan, BITMAPONLY + BITMAP2 )
 		return 1
 	end
 
 	if severity <= 50 then
 		this:explode( this.base, BITMAPONLY + BITMAP1 )
-		this:explode( this.smoke, FALL + BITMAP2 )
-		this:explode( this.pieces, BITMAPONLY + BITMAP2 )
+		this:explode( this.fan, FALL + BITMAP2 )
 		return 2
 	end
 
 	if severity <= 99 then
 		this:explode( this.base, BITMAPONLY + BITMAP1 )
-		this:explode( this.smoke, FALL + BITMAP2 )
-		this:explode( this.pieces, SHATTER + BITMAP2 )
+		this:explode( this.fan, FALL + BITMAP2 )
 		return 3
 	end
 
 	this:explode( this.base, BITMAPONLY + BITMAP1 )
-	this:explode( this.smoke, FALL + SMOKE + FIRE + EXPLODE_ON_HIT + BITMAP2 )
-	this:explode( this.pieces, SHATTER + EXPLODE_ON_HIT + BITMAP2 )
+	this:explode( this.fan, FALL + SMOKE + FIRE + EXPLODE_ON_HIT + BITMAP2 )
 	return 3
 end
 
