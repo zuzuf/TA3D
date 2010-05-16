e4edcd217571ff41ab0b0cc13a25573e0fb366cb
zuzuf <zuzuf@826f9f71-dfac-4a73-829b-727f66ff95d4>
2010-05-16 12:27:40 +0000

    * pressing ESC in sub menus doesn't exit the game any more

--- trunk/src/ta3d/src/ingame/menus/netmenu.cpp
+++ trunk/src/ta3d/src/ingame/menus/netmenu.cpp
@@ -106,6 +106,12 @@ namespace Menus
 
 	void NetMenu::doFinalize()
 	{
+		// Wait for user to release ESC
+		while (key[KEY_ESC])
+		{
+			SleepMilliSeconds(TA3D_MENUS_RECOMMENDED_TIME_MS_FOR_RESTING);
+			poll_inputs();
+		}
 		clear_keybuf();
 		NetClient::destroyInstance();
 	}
