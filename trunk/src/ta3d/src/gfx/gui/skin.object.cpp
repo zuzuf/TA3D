45691ec15b26b1e4a2c372e96ef946a45a922630
zuzuf <zuzuf@826f9f71-dfac-4a73-829b-727f66ff95d4>
2010-05-18 11:18:39 +0000

    * fixed rendering of progress bars when the bar width is smaller than the borders

--- trunk/src/ta3d/src/gfx/gui/skin.object.cpp
+++ trunk/src/ta3d/src/gfx/gui/skin.object.cpp
@@ -103,18 +103,22 @@ namespace Gui
 
 	void SKIN_OBJECT::draw(const float X1, const float Y1, const float X2, const float Y2, const bool bkg) const
 	{
-		gfx->drawtexture(tex, X1, Y1, X1 + x1, Y1 + y1, 0.0f, 0.0f, t_x1, t_y1);
-		gfx->drawtexture(tex, X1 + x1, Y1, X2 + x2, Y1 + y1, t_x1, 0.0f, t_x2, t_y1);
-		gfx->drawtexture(tex, X2 + x2, Y1, X2, Y1 + y1, t_x2, 0.0f, 1.0f, t_y1);
+		const float l = X2 + x2 - X1 - x1;
+		const float f = l < 0.0f ? (X2 - X1) / (x1 - x2) : 1.0f;
+		const float fu = (1.0f - f * (1.0f - t_x2)) / t_x2;
 
-		gfx->drawtexture(tex, X1, Y1 + y1, X1 + x1, Y2 + y2, 0.0f, t_y1, t_x1, t_y2);
-		gfx->drawtexture(tex, X2 + x2, Y1 + y1, X2, Y2 + y2, t_x2, t_y1, 1.0f, t_y2);
+		gfx->drawtexture(tex, X1, Y1, X1 + f * x1, Y1 + y1, 0.0f, 0.0f, f * t_x1, t_y1);
+		gfx->drawtexture(tex, X1 + f * x1, Y1, X2 + f * x2, Y1 + y1, f * t_x1, 0.0f, fu * t_x2, t_y1);
+		gfx->drawtexture(tex, X2 + f * x2, Y1, X2, Y1 + y1, fu * t_x2, 0.0f, 1.0f, t_y1);
 
-		gfx->drawtexture(tex, X1, Y2 + y2, X1 + x1, Y2, 0.0f, t_y2, t_x1, 1.0f);
-		gfx->drawtexture(tex, X1 + x1, Y2 + y2, X2 + x2, Y2, t_x1, t_y2, t_x2, 1.0f);
-		gfx->drawtexture(tex, X2 + x2, Y2 + y2, X2, Y2, t_x2, t_y2, 1.0f, 1.0f);
+		gfx->drawtexture(tex, X1, Y1 + y1, X1 + f * x1, Y2 + y2, 0.0f, t_y1, f * t_x1, t_y2);
+		gfx->drawtexture(tex, X2 + f * x2, Y1 + y1, X2, Y2 + y2, fu * t_x2, t_y1, 1.0f, t_y2);
 
-		if (bkg)
+		gfx->drawtexture(tex, X1, Y2 + y2, X1 + f * x1, Y2, 0.0f, t_y2, f * t_x1, 1.0f);
+		gfx->drawtexture(tex, X1 + f * x1, Y2 + y2, X2 + f * x2, Y2, f * t_x1, t_y2, fu * t_x2, 1.0f);
+		gfx->drawtexture(tex, X2 + f * x2, Y2 + y2, X2, Y2, fu * t_x2, t_y2, 1.0f, 1.0f);
+
+		if (bkg && l > 0.0f)
 			gfx->drawtexture(tex, X1 + x1, Y1 + y1, X2 + x2, Y2 + y2, t_x1, t_y1, t_x2, t_y2);
 	}
 
