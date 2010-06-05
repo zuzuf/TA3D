d732595196035c8bf80b5d03bb589ed85b46b364
zuzuf <zuzuf@users.noreply.github.com>
2010-06-05 10:14:05 +0000

    * old campaign main menu rewritten with the new menu API

--- src/ta3d/src/menu.h
+++ src/ta3d/src/menu.h
@@ -40,8 +40,6 @@ void network_room(void);		// Everything you need to host/join a network game
 
 void wait_room(void *p_game_data);			// Wait until everyone has loaded the game
 
-void campaign_main_menu(void);		// The campaign main menu, select the campaign you want to play
-
 Battle::Result brief_screen(String campaign_name, int mission_id);			// The brief screen where you are told about your mission objectives
 
 } // namespace Ta3D
