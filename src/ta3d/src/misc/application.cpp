b6f98354adf4c56a38ca9db9fe8888f258917999
zuzuf <zuzuf@users.noreply.github.com>
2010-06-06 00:01:28 +0000

    * fixed crash on exit

--- src/ta3d/src/misc/application.cpp
+++ src/ta3d/src/misc/application.cpp
@@ -39,7 +39,9 @@ namespace TA3D
 
 		// SDL_Quit(); // Should not be needed since SDL_Quit is installed as an atexit() routine
 
-		delete TA3D::VARS::lp_CONFIG;
+		if (TA3D::VARS::lp_CONFIG)
+			delete TA3D::VARS::lp_CONFIG;
+		TA3D::VARS::lp_CONFIG = NULL;
 
 		I18N::Destroy();
 		logs.info() << "Exiting now.";
